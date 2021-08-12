19773c2595807248fa416a585875f0e84d357e74
CristianCala <cristiancala@protonmail.com>
2021-08-12 09:50:02 -0400

    Delete fontawesome folder and add npm packages

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// paquetes de fontawesome y boostrap
+
+var bootstrap = require('bootstrap/dist/js/bootstrap');
+window.bootstrap = bootstrap;
+
+var fonta2 = require("@fortawesome/fontawesome-free/js/all");
+window.fonta3 = fonta2;
